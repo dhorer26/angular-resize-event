fce43364a53f632b2fedbd9b5891560c42684466
Ravindra Dhore <ravindrad@ansh.com>
2019-11-25 17:21:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ResizeEvent } from 'angular-resizable-element';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'simple-routing';
+  public style: object = {};
+
+  validate(event: ResizeEvent): boolean {
+    console.log(event);
+    const MIN_DIMENSIONS_PX = 50;
+    if (
+      event.rectangle.width &&
+      event.rectangle.height &&
+      (event.rectangle.width < MIN_DIMENSIONS_PX ||
+        event.rectangle.height < MIN_DIMENSIONS_PX)
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  onResizeEnd(event: ResizeEvent): void {
+    this.style = {
+      left: event.rectangle.left + 'px',
+      top: event.rectangle.top + 'px',
+      width: event.rectangle.width + 'px',
+      height: event.rectangle.height + 'px'
+    };
+    console.log(this.style);
+  }
 }
